0cb0f57704319e1100d3c5c37d76fb88528412e1
jasonbahl <jasonbahl@mac.com>
2016-10-28 22:22:21 -0600

    - moved Article Preview into it's own component

--- src/components/ArticleList/index.js
+++ src/components/ArticleList/index.js
@@ -1,16 +1,13 @@
 import React, { Component } from 'react';
 
+import ArticlePreview from '../ArticlePreview';
+
 class ArticleList extends Component {
 	render() {
 		return(
 
 			<div className="list-group item-list">
-				<a href="#" className="list-group-item list-group-item-action">
-					<h4 className="list-group-item-text p-t-1 p-b-1 small">Site 1</h4>
-	                <h5 className="list-group-item-heading">Article Title</h5>
-	                <h4 className="list-group-item-text p-t-1 small">November 5, 2016</h4>
-	                <p className="list-group-item-text p-t-1">This is the article excerpt</p>
-	            </a>
+				<ArticlePreview />
             </div>
 		)
 	}
