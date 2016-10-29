df91a311cf83ad94f3387c43a74f7a5d2e13597d
jasonbahl <jasonbahl@mac.com>
2016-10-28 22:16:17 -0600

    - move ArticleList into it's own component

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 
 import NavBar from './components/NavBar';
+import ArticleList from './components/ArticleList';
 
 class App extends Component {
 
@@ -19,16 +20,7 @@ class App extends Component {
                     <div className="row">
                         <div className="col-sm-4 p-l-0 p-r-0">
 
-                            <div className="list-group item-list">
-
-								<a href="#" className="list-group-item list-group-item-action">
-					                <h4 className="list-group-item-text p-t-1 p-b-1 small">Site 1</h4>
-					                <h5 className="list-group-item-heading">Article Title</h5>
-					                <h4 className="list-group-item-text p-t-1 small">November 5, 2016</h4>
-					                <p className="list-group-item-text p-t-1">This is the article excerpt</p>
-					            </a>
-
-                            </div>
+                            <ArticleList />
 
                         </div>
                         <div className="col-sm-8 p-r-0">
