d69d4e71d7311f9fe34bad941802b4a47a3b153c
jasonbahl <jasonbahl@mac.com>
2016-10-29 09:06:53 -0600

    This hooks up the ArticlePreview component to be aware of the "settings" Redux subtree and conditionally renders the site, date and excerpt based on their settings. So now, you can click the options in Preview Settings menu and the ArticlePreview component will respond and re-render appropriately. Awesome!

--- src/components/ArticlePreview/index.js
+++ src/components/ArticlePreview/index.js
@@ -1,16 +1,40 @@
+/**
+ * The article preview component renders details about the article, but what is displayed can be controlled by
+ * the "Preview Settings" in the navbar (or potentially any other component that triggers the settings to update their values).
+ * So this component needs to be aware of the settings in the Redux store, so that whenever anything causes the
+ * settings to change, this component will re-render accordingly.
+ */
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class ArticlePreview extends Component {
 	render() {
+
+		// Get the settings from the connected Redux store
+		let { settings } = this.props;
+
+		console.log( settings );
+
 		return(
 			<a href="#" className="list-group-item list-group-item-action">
-				<h4 className="list-group-item-text p-t-1 p-b-1 small">Site 1</h4>
+
+				{ (settings.showSite) ? <h4 className="list-group-item-text p-t-1 p-b-1 small">Site 1</h4> : '' }
                 <h5 className="list-group-item-heading">Article Title</h5>
-                <h4 className="list-group-item-text p-t-1 small">November 5, 2016</h4>
-                <p className="list-group-item-text p-t-1">This is the article excerpt</p>
+                { ( settings.showDate ) ? <h4 className="list-group-item-text p-t-1 small">November 5, 2016</h4> : '' }
+                { ( settings.showExcerpt ) ? <p className="list-group-item-text p-t-1">This is the article excerpt</p> : '' }
+
             </a>
 		)
 	}
 }
 
-export default ArticlePreview;
+/**
+ * Define which parts of the Redux store the component should be connected to
+ */
+function mapStateToProps( state ) {
+	return {
+		settings: state.settings
+	}
+}
+
+export default connect( mapStateToProps )(ArticlePreview);
