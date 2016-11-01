05a570a66743cd80c95c6f8531e74b812e4a8de3
jasonbahl <jasonbahl@mac.com>
2016-11-01 16:08:17 -0600

    Wire up the ArticleDetail component with dynamic data from the Redux store

--- src/components/ArticleDetail/index.js
+++ src/components/ArticleDetail/index.js
@@ -1,16 +1,61 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import moment from 'moment';
+import 'moment-timezone';
 
 class ArticleDetail extends Component {
-	render() {
-		return(
-			<div>
-                <h1>Article Title</h1>
-                <h4 className="small">November 5, 2016</h4>
-                <hr/>
-                <div>This is the full article content</div>
-            </div>
-		)
+
+    getActiveArticle() {
+
+        let { articles } = this.props;
+        let activeArticle = articles.items[0];
+
+        if ( articles.activeArticle ) {
+
+            articles.items.map(( article, i ) => {
+
+                if ( article.uniqueIndex === articles.activeArticle ) {
+                    activeArticle = article;
+                }
+                return activeArticle;
+
+            });
+        }
+
+        return activeArticle;
+
+
+    }
+
+    render() {
+
+        let { articles } = this.props;
+
+        if ( false === articles.activeArticle ) {
+            return(
+                <div>Loading...</div>
+            )
+        } else {
+
+            let article = this.getActiveArticle();
+            let date = ( article.date ) ? moment( article.date_gmt ).tz( article.timezone ).format('MM/DD/YYYY h:mm a z') : '';
+
+            return (
+                <div>
+                    <h1 dangerouslySetInnerHTML={{__html: article.title.rendered}}/>
+                    <h4 className="small" dangerouslySetInnerHTML={{__html: date}}/>
+                    <hr/>
+                    <div dangerouslySetInnerHTML={{__html: article.content.rendered}}/>
+                </div>
+            )
+        }
 	}
 }
 
-export default ArticleDetail;
+function mapStateToProps( state ) {
+    return {
+        articles: state.articles
+    }
+}
+
+export default connect( mapStateToProps )(ArticleDetail);
